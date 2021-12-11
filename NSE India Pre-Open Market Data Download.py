b4f9815478ae3db3c5c64a81cbaf7c88ae5394c8
TK <tkanhe@gmail.com>
2021-12-11 19:53:29 +0530

    Update NSE India Pre-Open Market Data Download.py
    https://howutrade.in does not allow to download data before the first day of the current month.

--- NSE India Pre-Open Market Data Download.py
+++ NSE India Pre-Open Market Data Download.py
@@ -3,21 +3,21 @@ import requests
 from datetime import datetime, timedelta
 
 
-def data_download(date_from='11-May-2018'):
+def data_download():
     """
-    :return: CSV files of F&O Pre-Open market data from a given date (data is available from 11-May-2018) to today's date.
+    Get F&O Pre-Open market data from the first day of the current month to today's date.
+    CSV files are saved in the "Preopen_Market_Data" folder in the current path.
     """
 
-    last_date = datetime.strptime(date_from, '%d-%b-%Y').date()  # datetime.date(2018, 5, 11)
-    current_date = datetime.today().date()  # datetime.date(2021, 11, 28)
-    no_of_days = (current_date - last_date).days  # 1297
+    last_date = datetime.now().replace(day=1).date()
+    current_date = datetime.today().date()
+    no_of_days = (current_date - last_date).days
 
     s = requests.Session()
     os.makedirs("Preopen_Market_Data", exist_ok=True)
 
     for i in range(no_of_days + 1):
-        url = f'https://howutrade.in/snapdata/?data=PreOpen_FO_{(last_date + timedelta(days=i)).strftime("%d%b%Y")}&export=csv'
-        r = s.get(url)
+        r = s.get(f'https://howutrade.in/snapdata/?data=PreOpen_FO_{(last_date + timedelta(days=i)).strftime("%d%b%Y")}&export=csv')
         if r.status_code == 200:  # On Market holidays you will get 404 status code
             with open(f"Preopen_Market_Data\\PreOpen_FO_{(last_date + timedelta(days=i)).strftime('%y.%m.%d')}.csv", 'wb') as f:
                 f.write(r.content)
